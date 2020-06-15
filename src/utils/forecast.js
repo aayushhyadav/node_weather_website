f1aa107e4afbd5415a2e1534d5f6d448bb1d9039
Aayush <aayushmum@gmail.com>
2020-06-15 10:08:30 +0530

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else{
-            console.log(latitude + ', ' + longitude)
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
         }       
      
