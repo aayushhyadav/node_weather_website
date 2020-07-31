0cbaf8f0a3f87a7bc8cb70f1d61bb02b28ed3fb8
Aayush <aayushmum@gmail.com>
2020-07-31 13:26:33 +0530

    Today's forecast feature added

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const myForecast = document.querySelector('#myForecast')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -21,4 +22,31 @@ weatherForm.addEventListener('submit', (e) => {
             }
         })
     })  
+})
+
+myForecast.addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    // disable the button
+    myForecast.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ' '
+
+        fetch('/weather?address=' + position.coords.longitude + ',' + position.coords.latitude)
+            .then((response) => {
+                response.json().then((data) => {
+                    if(data.error){
+                        messageOne.textContent = data.error
+                    }
+                    else{
+                        messageOne.textContent = data.location
+                        messageTwo.textContent = data.forecast
+                    }
+                })
+            })
+    })
 })
